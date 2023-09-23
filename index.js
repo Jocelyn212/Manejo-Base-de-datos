1b455e786d59dba234e3fddce1d7cdb52352c9d8
Jocelyn <129129841+Jocelyn212@users.noreply.github.com>
2023-09-23 05:40:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
